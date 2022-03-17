917903a49484f36fb2a45d5cb15d6775890851fd
Arcenio Souza <arcenio_neto@icloud.com>
2022-03-17 10:12:56 -0300

    Adiciona o componente Header

---
+++ src/components/header/StyledHeader.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+   height: 100px;
+   background-color: #ffff;
+   border-bottom: 1px solid #000;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+
+   img{
+      width: 15%;
+      height: 80%;
+   }
+`;
